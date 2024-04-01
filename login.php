43e8828eccf12b00cb08d54c328e0818bafa7cfc
vokkitt <114688805+vokkitt@users.noreply.github.com>
2024-04-01 23:55:29 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,99 @@
+<?php require_once('header.php'); ?>
+<!-- fetching row banner login -->
+<?php
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+foreach ($result as $row) {
+    $banner_login = $row['banner_login'];
+}
+?>
+<!-- login form -->
+<?php
+if(isset($_POST['form1'])) {
+        
+    if(empty($_POST['cust_email']) || empty($_POST['cust_password'])) {
+        $error_message = LANG_VALUE_132.'<br>';
+    } else {
+        
+        $cust_email = strip_tags($_POST['cust_email']);
+        $cust_password = strip_tags($_POST['cust_password']);
+
+        $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_email=?");
+        $statement->execute(array($cust_email));
+        $total = $statement->rowCount();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        foreach($result as $row) {
+            $cust_status = $row['cust_status'];
+            $row_password = $row['cust_password'];
+        }
+
+        if($total==0) {
+            $error_message .= LANG_VALUE_133.'<br>';
+        } else {
+            //using MD5 form
+            if( $row_password != md5($cust_password) ) {
+                $error_message .= LANG_VALUE_139.'<br>';
+            } else {
+                if($cust_status == 0) {
+                    $error_message .= LANG_VALUE_148.'<br>';
+                } else {
+                    $_SESSION['customer'] = $row;
+                    header("location: ".BASE_URL."dashboard.php");
+                }
+            }
+            
+        }
+    }
+}
+?>
+
+<div class="page-banner" style="background-color:#444;">
+    <div class="inner">
+        <h1><?php echo "Đăng nhập" ?></h1>
+    </div>
+</div>
+
+<div class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="user-content">
+
+                    
+                    <form action="" method="post">
+                        <?php $csrf->echoInputField(); ?>                  
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <?php
+                                if($error_message != '') {
+                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
+                                }
+                                if($success_message != '') {
+                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
+                                }
+                                ?>
+                                <div class="form-group">
+                                    <label for=""><?php echo "Email" ?> *</label>
+                                    <input type="email" class="form-control" name="cust_email">
+                                </div>
+                                <div class="form-group">
+                                    <label for=""><?php echo "Mật khẩu" ?> *</label>
+                                    <input type="password" class="form-control" name="cust_password">
+                                </div>
+                                <div class="form-group">
+                                    <label for=""></label>
+                                    <input type="submit" class="btn btn-search" style="padding:10px 30px" value="<?php echo "ĐĂNG NHẬP" ?>" name="form1">
+                                </div>
+                                <a href="forget-password.php" style="color:grey;"><?php echo "Quên mật khẩu ?" ?></a>
+                            </div>
+                        </div>                        
+                    </form>
+                </div>                
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('footer.php'); ?>
