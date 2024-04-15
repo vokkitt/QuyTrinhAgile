2eb2469f74cd7a4e3840a62c8d66c5cf9d39c37b
vokkitt <114688805+vokkitt@users.noreply.github.com>
2024-04-15 23:22:54 +0700

    Add files via upload
    SS-45: admin login

--- login.php
+++ login.php
@@ -1,99 +1,117 @@
-<?php require_once('header.php'); ?>
-<!-- fetching row banner login -->
 <?php
-$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
-$statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
-foreach ($result as $row) {
-    $banner_login = $row['banner_login'];
-}
-?>
-<!-- login form -->
-<?php
+ob_start();
+session_start();
+include("inc/config.php");
+include("inc/functions.php");
+include("inc/CSRF_Protect.php");
+$csrf = new CSRF_Protect();
+$error_message='';
+
 if(isset($_POST['form1'])) {
         
-    if(empty($_POST['cust_email']) || empty($_POST['cust_password'])) {
-        $error_message = LANG_VALUE_132.'<br>';
+    if(empty($_POST['email']) || empty($_POST['password'])) {
+        $error_message = 'Email and/or Password can not be empty<br>';
     } else {
-        
-        $cust_email = strip_tags($_POST['cust_email']);
-        $cust_password = strip_tags($_POST['cust_password']);
+		
+		$email = strip_tags($_POST['email']);
+		$password = strip_tags($_POST['password']);
 
-        $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_email=?");
-        $statement->execute(array($cust_email));
-        $total = $statement->rowCount();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        foreach($result as $row) {
-            $cust_status = $row['cust_status'];
-            $row_password = $row['cust_password'];
-        }
-
+    	$statement = $pdo->prepare("SELECT * FROM tbl_user WHERE email=? AND status=?");
+    	$statement->execute(array($email,'Active'));
+    	$total = $statement->rowCount();    
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);    
         if($total==0) {
-            $error_message .= LANG_VALUE_133.'<br>';
-        } else {
-            //using MD5 form
-            if( $row_password != md5($cust_password) ) {
-                $error_message .= LANG_VALUE_139.'<br>';
-            } else {
-                if($cust_status == 0) {
-                    $error_message .= LANG_VALUE_148.'<br>';
-                } else {
-                    $_SESSION['customer'] = $row;
-                    header("location: ".BASE_URL."dashboard.php");
-                }
+            $error_message .= 'Email Address does not match<br>';
+        } else {       
+            foreach($result as $row) { 
+                $row_password = $row['password'];
             }
+        
+            if( $row_password != md5($password) ) {
+                $error_message .= 'Password does not match<br>';
+            } else {       
             
+				$_SESSION['user'] = $row;
+                header("location: index.php");
+            }
         }
     }
+
+    
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Login</title>
 
-<div class="page-banner" style="background-color:#444;">
-    <div class="inner">
-        <h1><?php echo "Đăng nhập" ?></h1>
-    </div>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/ionicons.min.css">
+	<link rel="stylesheet" href="css/datepicker3.css">
+	<link rel="stylesheet" href="css/all.css">
+	<link rel="stylesheet" href="css/select2.min.css">
+	<link rel="stylesheet" href="css/dataTables.bootstrap.css">
+	<link rel="stylesheet" href="css/AdminLTE.min.css">
+	<link rel="stylesheet" href="css/_all-skins.min.css">
+
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body class="hold-transition login-page sidebar-mini">
+
+<div class="login-box">
+	<div class="login-logo">
+		<b>Admin Panel</b>
+	</div>
+  	<div class="login-box-body">
+    	<p class="login-box-msg">Log in to start your session</p>
+    
+	    <?php 
+	    if( (isset($error_message)) && ($error_message!='') ):
+	        echo '<div class="error">'.$error_message.'</div>';
+	    endif;
+	    ?>
+
+		<form action="" method="post">
+			<?php $csrf->echoInputField(); ?>
+			<div class="form-group has-feedback">
+				<input class="form-control" placeholder="Email address" name="email" type="email" autocomplete="off" autofocus>
+			</div>
+			<div class="form-group has-feedback">
+				<input class="form-control" placeholder="Password" name="password" type="password" autocomplete="off" value="">
+			</div>
+			<div class="row">
+				<div class="col-xs-8"></div>
+				<div class="col-xs-4">
+					<input type="submit" class="btn btn-success btn-block btn-flat login-button" name="form1" value="Log In">
+				</div>
+			</div>
+		</form>
+	</div>
 </div>
 
-<div class="page">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="user-content">
 
-                    
-                    <form action="" method="post">
-                        <?php $csrf->echoInputField(); ?>                  
-                        <div class="row">
-                            <div class="col-md-4"></div>
-                            <div class="col-md-4">
-                                <?php
-                                if($error_message != '') {
-                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
-                                }
-                                if($success_message != '') {
-                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
-                                }
-                                ?>
-                                <div class="form-group">
-                                    <label for=""><?php echo "Email" ?> *</label>
-                                    <input type="email" class="form-control" name="cust_email">
-                                </div>
-                                <div class="form-group">
-                                    <label for=""><?php echo "Mật khẩu" ?> *</label>
-                                    <input type="password" class="form-control" name="cust_password">
-                                </div>
-                                <div class="form-group">
-                                    <label for=""></label>
-                                    <input type="submit" class="btn btn-search" style="padding:10px 30px" value="<?php echo "ĐĂNG NHẬP" ?>" name="form1">
-                                </div>
-                                <a href="forget-password.php" style="color:grey;"><?php echo "Quên mật khẩu ?" ?></a>
-                            </div>
-                        </div>                        
-                    </form>
-                </div>                
-            </div>
-        </div>
-    </div>
-</div>
+<script src="js/jquery-2.2.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap.min.js"></script>
+<script src="js/select2.full.min.js"></script>
+<script src="js/jquery.inputmask.js"></script>
+<script src="js/jquery.inputmask.date.extensions.js"></script>
+<script src="js/jquery.inputmask.extensions.js"></script>
+<script src="js/moment.min.js"></script>
+<script src="js/bootstrap-datepicker.js"></script>
+<script src="js/icheck.min.js"></script>
+<script src="js/fastclick.js"></script>
+<script src="js/jquery.sparkline.min.js"></script>
+<script src="js/jquery.slimscroll.min.js"></script>
+<script src="js/app.min.js"></script>
+<script src="js/demo.js"></script>
 
-<?php require_once('footer.php'); ?>
+</body>
+</html>
